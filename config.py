19397198a9e9777d80e9ca72f45f4556133b4cd3
shema <shemaeric01@gmail.com>
2018-11-26 19:55:10 +0200

    updating config

--- config.py
+++ config.py
@@ -1,23 +1,22 @@
 import os
 
 class Development(object):
-
-  DEBUG = True
-  TESTING = True
-  JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
-  SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    """
+    Development environment configuration
+    """
+    DEBUG = True
+    TESTING = False
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
 
 class Production(object):
-
-  DEBUG = False
-  TESTING = False
-  SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
-  JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
-
-  ACCEPT = True
-  PENDING = False
-  VOTES = 0
-  DOWNVOTE = VOTES - 1
+    """
+    Production environment configurations
+    """
+    DEBUG = False
+    TESTING = False
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
 
 app_config = {
     'development': Development,
